1f84227086ef1493d42cc872e51d725ee5b8f664
Ahmed Hany <ahmed.hany_01@outlook.com>
2021-02-15 23:00:20 -0800

    added player and drew him on the screen

--- pygametest.py
+++ pygametest.py
@@ -2,17 +2,32 @@ import pygame
 
 pygame.init()
 
+# Creating the screen
 screen = pygame.display.set_mode((800, 600))
+
+# Setting the screen caption and icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("E:\Downloads\project.png")
 pygame.display.set_icon(icon)
 
+# Creating the player
+player_img = pygame.image.load("space-invaders.png")
+player_x = 370
+player_y = 480
+
+def player():
+    screen.blit(player_img, (player_x, player_y))
+
+# Game loop
 running = True
 
 while running:
+
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    player()
     pygame.display.update()
