19533716db01f57af31e7000aac43372c0eb24f1
Ahmed Hany <ahmed.hany_01@outlook.com>
2021-02-20 14:40:54 -0800

    added an enemy and a background, adding a bullet

--- pygametest.py
+++ pygametest.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 pygame.init()
 
@@ -10,6 +10,9 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("E:\Downloads\project.png")
 pygame.display.set_icon(icon)
 
+# Background
+background = pygame.image.load("background.png")
+
 # Creating the player
 player_img = pygame.image.load("space-invaders.png")
 player_x = 370
@@ -19,12 +22,35 @@ player_x_change = 0
 def player(x, y):
     screen.blit(player_img, (x, y))
 
+
+# Creating the enemy
+enemy_img = pygame.image.load("alien.png")
+enemy_x = random.randint(0, 800)
+enemy_y = random.randint(0, 50)
+enemy_x_change = 4
+enemy_y_change = 40
+
+def enemy(x, y):
+    screen.blit(enemy_img, (x, y))
+
+
+# Bullet
+bullet = pygame.image.load("bullet.png")
+#player_x = 370
+#player_y = 480
+#player_x_change = 0
+
+
 # Game loop
 running = True
 
 while running:
 
+    # RGB
     screen.fill((0, 0, 0))
+
+    # Background image
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,14 +58,33 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player_x_change = -0.3
+                player_x_change = -5
             if event.key == pygame.K_RIGHT:
-                player_x_change = 0.3
+                player_x_change = 5
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player_x_change = 0
 
+
+    #
     player_x += player_x_change
+
+    if player_x <= 0:
+        player_x = 0
+    elif player_x >= 736:
+        player_x = 736
+
+    #
+    enemy_x += enemy_x_change
+
+    if enemy_x <= 0:
+        enemy_x_change = 4
+        enemy_y += enemy_y_change
+    elif enemy_x >= 736:
+        enemy_x_change = -4
+        enemy_y += enemy_y_change
+
     player(player_x, player_y)
+    enemy(enemy_x, enemy_y)
     pygame.display.update()
